f9b8bcb3715902b5a42cb1667c9b5182c3301de9
Ori Elbaz <83533131+OriElbaz@users.noreply.github.com>
2026-01-14 18:29:18 +1100

    feat: add basic server components

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OriElbaz/chirpy
+
+go 1.25.5
--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-
+import (
+	"net/http"
+	"time"
+)
 
 
 func main(){
+	serveMux := http.NewServeMux()
 
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        serveMux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
 }
