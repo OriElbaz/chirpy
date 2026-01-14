2cec6d968efdf6ef806ffff1c45f40ba4f8f67dc
Ori Elbaz <83533131+OriElbaz@users.noreply.github.com>
2026-01-14 18:40:17 +1100

    feat: add index.html port

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 
 
 func main(){
+	// serveMux is a HTTP request router
 	serveMux := http.NewServeMux()
 
+	// http.Server is a struct that defines a server configuration
 	s := &http.Server{
 		Addr:           ":8080",
 		Handler:        serveMux,
@@ -17,5 +19,8 @@ func main(){
 		MaxHeaderBytes: 1 << 20,
 	}
 
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	
+	// ListenAndServe() blocks the main function until the server shuts down
 	s.ListenAndServe()
 }
