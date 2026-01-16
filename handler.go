c45b3e815ad5d9627b6740ac36515fcd7f7f792d
Ori Elbaz <83533131+OriElbaz@users.noreply.github.com>
2026-01-16 12:53:19 +1100

    feat: authentication functions

--- handler.go
+++ handler.go
@@ -35,6 +35,7 @@ type Chirp struct {
 }
 
 
+/***** HANDLER *****/
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         cfg.fileserverHits.Add(1)
